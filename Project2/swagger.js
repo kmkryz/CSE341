583c38f707540843b7664c4ecf3408bc742fd967
kmkryz <kmkryz@gmail.com>
2024-11-25 11:43:28 -0500

    swagger update

--- Project2/swagger.js
+++ Project2/swagger.js
@@ -1,14 +1,26 @@
 const swaggerAutogen = require('swagger-autogen')();
 
 const doc = {
+    swagger: "2.0",
     info: {
         title: 'Recipe API',
         description: 'Recipe API Documentation',
+        version: '1.0.0'
     },
     host: process.env.NODE_ENV === 'production' 
-        ? 'recipe-api-qqxm.onrender.com'  // Replace with your actual Render.com URL
+        ? 'recipe-api-qqxm.onrender.com'
         : 'localhost:8080',
     schemes: process.env.NODE_ENV === 'production' ? ['https'] : ['http'],
+    securityDefinitions: {
+        oauth2: {
+            type: 'oauth2',
+            flow: 'implicit',
+            authorizationUrl: 'https://github.com/login/oauth/authorize',
+            scopes: {
+                'user:email': 'Read user email address'
+            }
+        }
+    }
 };
 
 const outputFile = './swagger.json';
